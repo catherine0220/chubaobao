100d9efa3b1f5c6f0595bba1dd95a92b80bef81b
catherine0220 <catherineong0220@gmail.com>
2025-06-04 15:51:38 +0800

    remove unocss and update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import UnoCSS from 'unocss/vite'
-import path from 'path' // 👈 加上这个
+import path from 'path'
 
 export default defineConfig({
-  plugins: [vue(), UnoCSS()],
+  plugins: [vue()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'), // 👈 重点就在这里
+      '@': path.resolve(__dirname, './src'),
     },
     extensions: ['.js', '.vue', '.json']
   }
