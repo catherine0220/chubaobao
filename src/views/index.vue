cc54aa20e704ac7b8ddc82437131e159ae064c0b
catherine0220 <catherineong0220@gmail.com>
2025-06-05 11:07:58 +0800

    completed promo

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,32 +1,51 @@
 <template>
-<div class="body">
 	<div class="promo-container">
 			<div class="promo-tile">
 				<div class="promo-content">
-				<p class="promo-title">投资金额：1000-5000</p>
-				<p class="promo-text">投资金额：1000-5000</p>
-				<button class="promo-button">11</button>
+				<p class="promo-title">投资金额：<span class="highlight">1000-5000</span></p>
+				<p class="promo-text">每月收入</p>
+				<p class="promo-text2">
+					<span class="part1">2%</span>
+					<span class="part2">180天</span>
+				</p>
+				<button class="promo-button">
+					购买
+					<img src="@/assets/img/arrow-right.png" alt="buttonarrow" class="button-arrow"/>
+				</button>
 				</div>
 				<img class="promo-img" src="@/assets/img/promo1.png" alt="promo" />
 			</div>
 			<div class="promo-tile">
 				<div class="promo-content">
-				<h3 class="promo-title">投资金额：1000-5000</h3>
-				<p class="promo-text">投资金额：1000-5000</p>
-				<button class="promo-button">11</button>
+				<h3 class="promo-title">投资金额：<span class="highlight">50000-10000</span></h3>
+				<p class="promo-text">每月收入</p>
+				<p class="promo-text2">
+					<span class="part1">2.5%</span>
+					<span class="part2">180天</span>
+				</p>
+				<button class="promo-button">
+					购买
+					<img src="@/assets/img/arrow-right.png" alt="buttonarrow" class="button-arrow" />
+				</button>
 				</div>
 				<img class="promo-img" src="@/assets/img/promo2.png" alt="promo" />
 			</div>
 			<div class="promo-tile">
 				<div class="promo-content">
-				<h3 class="promo-title">投资金额：1000-5000</h3>
-				<p class="promo-text">8,100+USDT</p>
-				<button class="promo-button">11</button>
+				<h3 class="promo-title">投资金额：<span class="highlight">10000-20000</span></h3>
+				<p class="promo-text">每月收入</p>
+				<p class="promo-text2">
+					<span class="part1">3%</span>
+					<span class="part2">180天</span>
+				</p>
+				<button class="promo-button">
+					购买
+					<img src="@/assets/img/arrow-right.png" alt="buttonarrow" class="button-arrow"/>
+				</button>
 				</div>
 				<img class="promo-img" src="@/assets/img/promo3.png" alt="promo" />
 			</div>
-	</div>
-</div>
+		</div>
 </template>
 
 <script setup>
